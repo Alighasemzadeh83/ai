f92af82178f22fc86f0c84f4c7a46eb433cbd07e
fraxea <moh788672@gmail.com>
2024-07-17 21:57:10 +0330

    Implement optimization

--- Tic Tac Toe/main.py
+++ Tic Tac Toe/main.py
@@ -1,9 +1,17 @@
 from env import tic_tac_toe
 from agent import net, visualize
 from worker.self_play import self_play
+from worker.optimize import train, load_best_model
+
+import torch
+
+import os
 
 
 H = {
+    "lr": 0.001,
+    "num_epochs": 10,
+    "batch_size": 32,
     "num_games": 1,
     "num_simulations": 50,
     "C": 1.0
@@ -11,5 +19,8 @@ H = {
 
 env = tic_tac_toe.TicTacToe()
 model = net.QNetwork(in_channels=3, action_space=env.action_space)
+model = load_best_model(model)
 
-self_play(model, env, num_games=H["num_games"], num_simulations=H["num_simulations"], C=H["C"])
+# self_play(model, env, num_games=H["num_games"], num_simulations=H["num_simulations"], C=H["C"])
+
+train(model, transform=env.transform, lr=H["lr"], num_epochs=H["num_epochs"], batch_size=H["batch_size"])
--- Tic Tac Toe/worker/optimize.py
+++ Tic Tac Toe/worker/optimize.py
@@ -3,11 +3,58 @@ Optimize the model using the experiences generated from self-play
 """
 
 import torch
+import torch.nn.functional as F
 from torch.utils.data import DataLoader, Dataset
 
 import os
 import numpy as np
 
+def train(model, transform, lr, num_epochs, batch_size):
+    optimizer = torch.optim.Adam(model.parameters(), lr=lr)
+    dataloader = get_dataloader(batch_size, transform)
+    for _ in range(num_epochs):
+        optimize(model, dataloader, optimizer)
+    
+    # save the model
+    os.makedirs("models", exist_ok=True)
+    # find the last generation
+    generation = 0
+    for root, dirs, files in os.walk("models"):
+        for file in files:
+            if file.endswith(".pth"):
+                gen = int(file.split("_")[1].split(".")[0])
+                if gen > generation:
+                    generation = gen
+    torch.save(model.state_dict(), f"models/generation_{generation + 1}.pth")
+
+
+def load_best_model(model):
+    os.makedirs("models", exist_ok=True)
+    generation = 0
+    for root, dirs, files in os.walk("models"):
+        for file in files:
+            if file.endswith(".pth"):
+                gen = int(file.split("_")[1].split(".")[0])
+                if gen > generation:
+                    generation = gen
+    if generation > 0:
+        model.load_state_dict(torch.load(f"models/generation_{generation}.pth"))
+    return model
+
+def optimize(model, dataloader, optimizer):
+    model.train()
+    for state, policy, value in dataloader:
+        optimizer.zero_grad()
+        policy_pred, value_pred = model(state)
+        policy_loss = F.cross_entropy(policy_pred, policy)
+        value_loss = F.mse_loss(value_pred, value)
+        loss = policy_loss + value_loss
+        loss.backward()
+        optimizer.step()
+
+def get_dataloader(batch_size, transform):
+    dataset = ExperienceDataset(transform=transform)
+    return DataLoader(dataset, batch_size=batch_size, shuffle=True)
 
 class ExperienceDataset(Dataset):
     def __init__(self, path=None, transform=None):
@@ -23,10 +70,9 @@ class ExperienceDataset(Dataset):
         return len(self.states)
 
     def __getitem__(self, idx):
-        state = torch.from_numpy(self.states[idx], dtype=torch.float32)
-        if self.transform:
-            state = self.transform(state)
-        policy = torch.from_numpy(self.policies[idx], dtype=torch.float32)
+        state = torch.from_numpy(self.states[idx]).float()
+        state = self.transform(state)
+        policy = torch.from_numpy(self.policies[idx]).float()
         value = torch.tensor(self.values[idx], dtype=torch.float32).unsqueeze(0)
         return state, policy, value
 
@@ -35,8 +81,6 @@ def find_newest_dataset():
     for root, dirs, files in os.walk("experiences"):
         for file in files:
             if file.endswith(".npz"):
-                path = os.path.join(root, file)
-                timestamp = int(os.path.basename(root))
-                if newest is None or timestamp > newest[0]:
-                    newest = (timestamp, path)
-    return newest[1]
+                if newest is None or file > newest:
+                    newest = file
+    return f"experiences/{newest}"
